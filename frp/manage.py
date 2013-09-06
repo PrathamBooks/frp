c407352102939405fec6a0f868faab7fada7c42f
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-09-06 12:11:56 +0530

    Adds an entry point script

---
+++ frp/manage.py
@@ -0,0 +1,17 @@
+from flask.ext.script import Manager
+
+from frp import app
+
+manager = Manager(app)
+
+@manager.command
+def resetdb():
+    from models import db
+    print "Dropping all tables"
+    db.drop_all()
+    print "Creating them afresh"
+    db.create_all()
+
+if __name__ == '__main__':
+    manager.run()
+
