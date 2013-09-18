6adf1d3b9609ee959c30cfe2b1500bd234c16b4b
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-09-18 15:55:45 +0530

    Adds some py.test infrastructure and dummy tests

--- frp/tests/conftest.py
+++ frp/tests/conftest.py
@@ -1,8 +1,23 @@
+import pytest
+
+from flask.ext.lastuser.sqlalchemy import UserManager
+
 from frp import app, lastuser, models
 
-def pytest_funcarg__testdb(request):
+@pytest.fixture
+def test_db(request):
     app.config.from_pyfile("settings/test.py")
     models.db.create_all()
     
+@pytest.fixture
+def test_client(request):
+    app.config.from_pyfile("settings/test.py")
+    tc = app.test_client()
+    return tc
 
+@pytest.fixture
+def last_user(request):
+    app.config.from_pyfile("settings/test.py")
+    lastuser.init_app(app)
+    lastuser.init_usermanager(UserManager(models.db, models.User))
 
--- frp/tests/test_basic.py
+++ frp/tests/test_basic.py
@@ -2,11 +2,25 @@ import os
 import unittest
 import tempfile
 
-from frp import app, lastuser, models
 
-def test_sample(testdb):
-    tc = app.test_client()
-    op = tc.get("/")
-    print op
+def test_sample(test_db, test_client):
+    "No products added at first"
+    op = test_client.get("/")
+    assert "No products available" in op.data
+
+def test_add_product(test_db, test_client, last_user):
+    "Adding a product to the database"
+    test_client.post('/login', data=dict(
+        username="noufal",
+        password="abcd",
+    ), follow_redirects=True)
+    op = test_client.get("/product/add")
+    print "Hel9"
+    print op.data
+    
+    
+    
+
+
 
     
