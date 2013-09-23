cc599f9a92092e16ed6d98603fd47f356c6d7030
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-09-23 17:54:32 +0530

    Uses live lastuser instance

--- frp/frp/settings/production.py
+++ frp/frp/settings/production.py
@@ -7,8 +7,8 @@ SECRET_KEY = "iLd3QjFqgGCJQ"
 SITE_NAME="Pratham Books Fund raising platform"
 
 #: LastUser server
-LASTUSER_SERVER = 'http://lastuser.io'
+LASTUSER_SERVER = 'http://frp-auth.prathambooks.org/'
 #: LastUser client id
-LASTUSER_CLIENT_ID = 'Eg7rNU2xRim8ZJ2nwCupeQ'
+LASTUSER_CLIENT_ID = 'fuSI9hmWQVuFHSaVpDLtCg'
 #: LastUser client secret
-LASTUSER_CLIENT_SECRET = 'oB-6wY-wSiqhtTJLdRUG-gz00oiFNyRuG2IlANh_Dvlg'
+LASTUSER_CLIENT_SECRET = 'gXemhFkUSmCGRCJn11NKqwL6O2o5N4S-mI69U2BfMi7w'
