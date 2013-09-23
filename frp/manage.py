a33122e32d77483083ce460d993b7726bf39ad9f
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-09-23 11:41:46 +0530

    Moves settings to global level

--- frp/manage.py
+++ frp/manage.py
@@ -9,22 +9,26 @@ from frp import app, models, lastuser
 
 manager = Manager(app)
 
+settings = "settings/development.py"
+settings = os.environ.get('FRP_CONFIG', settings)
+print "Loading config from %s"%settings
+
 @manager.command
-def runserver(settings = "settings/development.py"):
+def runserver():
     """
     Runs the flask development server using the specified config file. 
     
     Defaults to settings/development.py
-    """    
-    settings = os.environ.get('FRP_CONFIG', settings)
-    print "Loading config from %s"%settings
+    """
+    global settings
     app.config.from_pyfile(settings)
     lastuser.init_app(app)
     lastuser.init_usermanager(UserManager(models.db, models.User))
     app.run()
 
 @manager.command
-def resetdb(settings = "settings/development.py"):
+def resetdb():
+    global settings
     app.config.from_pyfile(settings)
     db = models.db
     lastuser = Lastuser()
