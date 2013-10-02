80930b95a3534d96d30c04458ea676981de0d4b1
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-10-02 13:44:22 +0530

    Adds web assets to manage static assets.
    Signed-off-by: Noufal Ibrahim <noufal@nibrahim.net.in>

--- frp/frp/__init__.py
+++ frp/frp/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask.ext.lastuser import Lastuser
+from flask.ext.assets import Environment, Bundle
 from ._version import __version__
 
 app = Flask(__name__)
+assets = Environment(app)
 lastuser = Lastuser()
+
+css = Bundle('style.css', filters='cssmin', output='gen/all.css')
+
+assets.register('css_site', css)
+
 
 @app.context_processor
 def inject_version():
     return dict(version = __version__,
                 testing = app.config.get('LASTUSER_TEST'))
-
 
 from . import views, models
