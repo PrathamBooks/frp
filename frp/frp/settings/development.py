67b405f183d1e84538ae34642b293d8eabbb05e5
Alok Kuchlous <kuchlous@gmail.com>
2013-10-28 17:59:01 +0530

    getting started and changes to access frp auth server

--- frp/frp/settings/development.py
+++ frp/frp/settings/development.py
@@ -1,5 +1,5 @@
 # Database settings
-SQLALCHEMY_DATABASE_URI = "postgresql://noufal:noufal@localhost/frp"
+SQLALCHEMY_DATABASE_URI = "postgresql://alok:alok@localhost/frp"
 
 DEBUG = True
 SECRET_KEY = "Development key"
@@ -7,11 +7,11 @@ SECRET_KEY = "Development key"
 SITE_NAME="Pratham Books Fund raising platform"
 
 #: LastUser server
-LASTUSER_SERVER = 'http://lastuser.io'
+LASTUSER_SERVER = 'http://frp-auth.prathambooks.org'
 #: LastUser client id
-LASTUSER_CLIENT_ID = 'Eg7rNU2xRim8ZJ2nwCupeQ'
+LASTUSER_CLIENT_ID = 'FUVs_77tS9S4wHl0DVvkqQ'
 #: LastUser client secret
-LASTUSER_CLIENT_SECRET = 'oB-6wY-wSiqhtTJLdRUG-gz00oiFNyRuG2IlANh_Dvlg'
+LASTUSER_CLIENT_SECRET = 'DKERE5jARl6bcnjo68e7EQDRI5XwCeTz6_3p4ly96oMA'
 
 # Bypass actual lastuser server 
-LASTUSER_TEST = True
+LASTUSER_TEST = False
