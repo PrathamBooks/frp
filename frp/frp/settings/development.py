d8e31b8cfeb2f6d91adf434a93ff3fb19790731f
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-10-29 14:16:12 +0530

    Adds information for both lastuser instances.
    Signed-off-by: Noufal Ibrahim <noufal@nibrahim.net.in>

--- frp/frp/settings/development.py
+++ frp/frp/settings/development.py
@@ -8,10 +8,14 @@ SITE_NAME="Pratham Books Fund raising platform"
 
 #: LastUser server
 LASTUSER_SERVER = 'http://frp-auth.prathambooks.org/'
+# LASTUSER_SERVER = 'http://lastuser.io/'
 #: LastUser client id
-LASTUSER_CLIENT_ID = 'z6Ke4mDiSBGxRvBurAvVkQ'
+LASTUSER_CLIENT_ID = 'z6Ke4mDiSBGxRvBurAvVkQ' # frp-auth client id
+# LASTUSER_CLIENT_ID = 'yhF5SHMIQ5C4ruQJqF9YTQ'   # lastuser.io client id
+
 #: LastUser client secret
 LASTUSER_CLIENT_SECRET = 'Rxog-fYPQWyHscQL_qGmSg7esBJCHnRoCOYiwfL_bLug'
+# LASTUSER_CLIENT_SECRET = 'LTUj8jdtRbCCej_PadT1zQM4cjTdbKTQOkNhDKl-90FA'
 
 # Bypass actual lastuser server 
-LASTUSER_TEST = True
+LASTUSER_TEST = False
