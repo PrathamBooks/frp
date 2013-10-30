850f1bca63a044e1735cd83d3e0f9d72dc5c1f1e
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-10-30 14:40:18 +0530

    Reorganise settings file.
    Signed-off-by: Noufal Ibrahim <noufal@nibrahim.net.in>

--- frp/frp/settings/development.py
+++ frp/frp/settings/development.py
@@ -6,15 +6,14 @@ SECRET_KEY = "Development key"
 
 SITE_NAME="Pratham Books Fund raising platform"
 
-#: LastUser server
+#: LastUser server (frp-auth)
 LASTUSER_SERVER = 'http://frp-auth.prathambooks.org/'
-# LASTUSER_SERVER = 'http://lastuser.io/'
-#: LastUser client id
-LASTUSER_CLIENT_ID = 'z6Ke4mDiSBGxRvBurAvVkQ' # frp-auth client id
-# LASTUSER_CLIENT_ID = 'yhF5SHMIQ5C4ruQJqF9YTQ'   # lastuser.io client id
-
-#: LastUser client secret
+LASTUSER_CLIENT_ID = 'z6Ke4mDiSBGxRvBurAvVkQ' 
 LASTUSER_CLIENT_SECRET = 'Rxog-fYPQWyHscQL_qGmSg7esBJCHnRoCOYiwfL_bLug'
+
+# Lastuser server (Lastuser.io / localhost)
+# LASTUSER_SERVER = 'http://lastuser.io/'
+# LASTUSER_CLIENT_ID = 'yhF5SHMIQ5C4ruQJqF9YTQ'   
 # LASTUSER_CLIENT_SECRET = 'LTUj8jdtRbCCej_PadT1zQM4cjTdbKTQOkNhDKl-90FA'
 
 # Bypass actual lastuser server 
