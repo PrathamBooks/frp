78aec8eed75dbebb53695bc4f397fa1fa9196d95
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-10-30 15:09:00 +0530

    Simple admin interface useful during development.
    Signed-off-by: Noufal Ibrahim <noufal@nibrahim.net.in>

--- frp/frp/views/admin.py
+++ frp/frp/views/admin.py
@@ -5,6 +5,10 @@ Admin interface
 from flask import g
 from flask.ext.admin import Admin, AdminIndexView, expose
 from flask.ext.admin.contrib.sqla import ModelView
+from flask.ext.admin.form import BaseForm
+
+from wtforms import TextField
+from wtforms.validators import DataRequired
 
 from .. import app
 from .. import models
@@ -18,23 +22,15 @@ class IndexView(AdminIndexView):
 
 class BaseAuthModelView(ModelView):
     def is_accessible(self):
-        return hasattr(g, "lastuserinfo")
-
-class UserModelView(BaseAuthModelView):
-    column_list = ('description', 'fullname', 'email')
-
-class CampaignModelView(BaseAuthModelView):
-    column_list = ('name', 'subheading', 'description', 'start', 'end', 'pledged', 'target', 'created_by', 'approved_by', 'approved_on', 'verified_by')
-
-class CategoryModelView(BaseAuthModelView):
-    column_list = ('name', 'icon', 'campaigns')
-
+        return hasattr(g, "lastuserinfo") and g.lastuserinfo
+    
+    
 
 admin = Admin(app, name="Pratham Fundraiser", index_view = IndexView(name="Home"))
-admin.add_view(UserModelView(models.User, models.db.session, endpoint = "user"))
-admin.add_view(CampaignModelView(models.Campaign, models.db.session, endpoint = "campaign" ))
-admin.add_view(CategoryModelView(models.Category, models.db.session, endpoint = "category" ))
-# admin.add_view(BaseAuthModelView(models.Image, models.db.session, endpoint = "image" ))
+admin.add_view(BaseAuthModelView(models.User, models.db.session, endpoint = "user"))
+admin.add_view(BaseAuthModelView(models.Campaign, models.db.session, endpoint = "campaign" ))
+admin.add_view(BaseAuthModelView(models.Category, models.db.session, endpoint = "category" ))
+admin.add_view(BaseAuthModelView(models.Image, models.db.session, endpoint = "image" ))
 
 
 
