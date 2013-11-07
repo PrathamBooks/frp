af3c38004d71e3ea8b9dd1b05a752ee81936aaeb
Alok Kuchlous <kuchlous@gmail.com>
2013-11-07 16:36:01 +0530

    Basic test setup along with auth bypass

---
+++ frp/tests/unit/test_add_product.py
@@ -0,0 +1,19 @@
+from frp import app, models, lastuser
+
+def test_add_product(test_db, test_client, last_user):
+    "Adding a product to the database"
+    app.lastuser_auth_data = dict(
+        token_scope = u'name email',
+        userid = u'1', 
+        username = u'alokk', 
+        email = u'kuchlous@gmail.com', 
+        fullname = u'Alok Kuchlous'
+        )
+    app.lastuser_auth_token = dict(
+        access_token = u'pAfGGWHzRtqRmdSQ_CdrWA',
+        token_type = u'bearer',
+        scope = u'email id')
+
+    test_client.post("/login", follow_redirects=True)
+    op = test_client.get("/product/add")
+    print op.data
--- frp/tests/unit/test_basic.py
+++ frp/tests/unit/test_basic.py
@@ -2,25 +2,9 @@ import os
 import unittest
 import tempfile
 
-
 def test_sample(test_db, test_client):
     "No products added at first"
     op = test_client.get("/")
     assert "No products available" in op.data
 
-def test_add_product(test_db, test_client, last_user):
-    "Adding a product to the database"
-    test_client.post('/login', data=dict(
-        username="noufal",
-        password="abcd",
-    ), follow_redirects=True)
-    op = test_client.get("/product/add")
-    print "Hel9"
-    print op.data
-    
-    
-    
-
-
-
-    
+   
