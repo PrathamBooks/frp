f0d1af645d682c4c2509afe7fee18153847d9a5a
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-11-11 14:21:17 +0530

    Use my database name in development file.
    Signed-off-by: Noufal Ibrahim <noufal@nibrahim.net.in>

--- frp/frp/settings/development.py
+++ frp/frp/settings/development.py
@@ -1,5 +1,5 @@
 # Database settings
-SQLALCHEMY_DATABASE_URI = "postgresql://alok:alok@localhost/frp"
+SQLALCHEMY_DATABASE_URI = "postgresql://noufal:noufal@localhost/frp"
 
 DEBUG = True
 SECRET_KEY = "Development key"
