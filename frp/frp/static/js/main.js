2a0bb695a293d133c6460ad288ee5a4e1402dbc9
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-11-12 18:47:47 +0530

    Use ajax for create category form.
    Signed-off-by: Noufal Ibrahim <noufal@nibrahim.net.in>

--- frp/frp/static/js/main.js
+++ frp/frp/static/js/main.js
@@ -1,4 +1,21 @@
+var ajaxify_category_form = function() {
+    $("#create_category").submit(function(event) {
+        var url = $("button[form='create_category']").attr("data-async-submit");
+        var data = $("#create_category").serializeArray();
+        $.post(url, data).done(
+            function(data, textStatus, jqXHR) {
+                $("div#message").text(data['message']).show().fadeOut(1000);
+            }).fail(function(jqXHR, textStatus, errorThrown) {
+                var resp = $.parseJSON(jqXHR.responseText);
+                var message = resp['errors'][0]['message'];
+                $("div#message").text(message).show().fadeOut(2000);
+            });
+        event.preventDefault();
+    });
+};
+
 $(function() {
     $("input.datepicker").datepicker({dateFormat : "yy-mm-dd"});
+    ajaxify_category_form();
 });
 
