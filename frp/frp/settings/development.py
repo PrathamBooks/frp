45e44d773c7a4ab58509a7bc7b9073ef87026413
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-11-18 13:16:00 +0530

    Changes password for my local db.
    Signed-off-by: Noufal Ibrahim <noufal@nibrahim.net.in>

--- frp/frp/settings/development.py
+++ frp/frp/settings/development.py
@@ -1,5 +1,5 @@
 # Database settings
-SQLALCHEMY_DATABASE_URI = "postgresql://noufal:noufal@localhost/frp"
+SQLALCHEMY_DATABASE_URI = "postgresql://noufal:abcdef@localhost/frp"
 
 DEBUG = True
 SECRET_KEY = "Development key"
