afc5371317f55b0af9e3244acdf718fe2e156a18
Kracekumar Ramaraju <me@kracekumar.com>
2014-10-04 21:45:19 +0530

    Fixed wsgi file

--- frp/wsgi.py
+++ frp/wsgi.py
@@ -1,12 +1,8 @@
 import os
 
-from flask.ext.lastuser.sqlalchemy import UserManager
-from frp import app, models, lastuser
+from frp import app
 
-settings="settings/production.py"
+settings = "settings/production.py"
 settings = os.environ.get('FRP_CONFIG', settings)
 
 app.config.from_pyfile(settings)
-lastuser.init_app(app)
-lastuser.init_usermanager(UserManager(models.db, models.User))
-
