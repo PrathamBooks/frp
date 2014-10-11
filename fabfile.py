142bf797a4f759d855813ecc379da71c34c60ded
hsenvig <hsenvig@gmail.com>
2014-10-11 15:03:53 +0530

    changes pep8 in fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,5 @@
-from fabric.api import run, cd, env, settings, sudo, local, shell_env
+from fabric.api import run, cd, env, sudo, local, shell_env
 from fabric.contrib.files import sed
-
 
 
 env.hosts = ['ubuntu@ec2-54-251-215-76.ap-southeast-1.compute.amazonaws.com']
@@ -13,48 +12,61 @@ deployments_dir = "{0}/deployments".format(app_base)
 currently_deployed_dir = "{0}/deployed".format(app_base)
 version_file = "{0}/frp/frp/_version.py".format(currently_deployed_dir)
 production_config_file = "/opt/config/deployed/frp/production.py"
-app_production_config_file = "{0}/frp/frp/settings/production.py".format(currently_deployed_dir)
+app_production_config_file = "{0}/frp/frp/settings/production.py".format(
+    currently_deployed_dir)
 
 virtualenv_home = "/opt/frp/environments"
 app_virtualenv = "frp-app"
 
-#Remote commands
+
+# Remote commands
 def obtain_code(tag):
     with cd(deployments_dir):
         run("rm -Rf {0}".format(tag))
         run("git clone -q {0} {1}".format(app_repository, tag))
         run("cd {0}/{1} && git checkout {1}".format(deployments_dir, tag))
-        
+
+
 def set_deployed_version(tag):
     with cd(app_base):
         run("rm -f {0}".format(currently_deployed_dir))
-        run("ln -s {0}/{1} {2}".format(deployments_dir, tag, currently_deployed_dir))
+        run("ln -s {0}/{1} {2}".format(deployments_dir, tag,
+                                       currently_deployed_dir))
         # Write version file which will get loaded by the application.
         sed(version_file, "development", tag)
-    
+
 
 def update_virtualenv(tag):
     with cd(app_base):
-        run(". {0}/{1}/bin/activate && pip install -M -r {2}/requirements/production.txt".format(virtualenv_home, app_virtualenv, currently_deployed_dir))
+        run(". {0}/{1}/bin/activate && "
+            "pip install -M -r {2}/requirements/production.txt".format(
+                virtualenv_home, app_virtualenv, currently_deployed_dir))
+
 
 def monit_restart():
     sudo("monit restart all")
 
+
 def reset_database():
-    with cd(currently_deployed_dir), shell_env(FRP_CONFIG = "settings/production.py"):
-        run(". {0}/{1}/bin/activate && python frp/manage.py resetdb".format(virtualenv_home, app_virtualenv))
+    with cd(currently_deployed_dir), shell_env(FRP_CONFIG="settings/production.py"):
+        run(". {0}/{1}/bin/activate && python frp/manage.py resetdb".format(
+            virtualenv_home, app_virtualenv))
+
 
 def fix_config():
     with cd(currently_deployed_dir):
-        run("ln -s {0} {1}".format(production_config_file, app_production_config_file))
+        run("ln -s {0} {1}".format(production_config_file,
+                                   app_production_config_file))
+
 
 # Local commands
 def push_code():
     local("git push --force")
     local("git push --tags")
 
+
 # Public commands
-def deploy(tag, venv = False, resetdb = False):
+def deploy(tag, venv=False, resetdb=False):
     "Deploy the application tagged by :tag:"
     push_code()
     obtain_code(tag)
@@ -66,8 +78,9 @@ def deploy(tag, venv = False, resetdb = False):
         reset_database()
     monit_restart()
 
+
 def rollback(tag):
     "Roll back to given tag"
     set_deployed_version(tag)
-    update_monit_config()
+    # update_monit_config()
     monit_restart()
