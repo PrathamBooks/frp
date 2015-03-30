9ed4bd7554947da19b147371d3750aab9205bb5a
Sairam Sudhir P <sairamsudhir@mirafra-ThinkCentre-A70.(none)>
2015-03-30 17:48:06 +0530

    Missed to add comment model

---
+++ frp/frp/models/comment.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import inspect
+from datetime import date
+
+from . import db, BaseNameMixin, BaseMixin
+
+__all__ = ['Comment']
+
+class Comment(BaseMixin, db.Model):
+    __tablename__ = 'comment'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    campaign_id = db.Column(db.Integer, db.ForeignKey('campaign.id'), nullable=False)
+    comment = db.Column(db.Unicode(500), nullable=False)
+
+
+    def get_comment(self):
+        return { "date": "{:%B %d, %Y}".format(self.start_date()),
+                "comment": self.comment
+               }
+    def start_date(self):
+        return self.created_at.date()
+
+    def commit(self):
+        db.session.add(self)
+        try:
+          db.session.commit()
+          print 'Successfull commit'
+          return 0
+        except Exception as e:
+          print "commit not done",e
+          return e
+          db.session.rollback()
