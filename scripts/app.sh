34dbd57eeea4cfb2e43843ca9015746b1e7c085b
Alok Kuchlous <kuchlous@gmail.com>
2015-04-03 03:27:04 +0530

    production settings

--- scripts/app.sh
+++ scripts/app.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-# Management script for FRP application Managed by system monit. 
+# Management script for FRP application Managed by system monit.
 
 # If you change any of these file locations, please make corresponding
 # changes in the /etc/monit/conf.d/frp.conf file on the server. The
 # local file scripts/monit/frp.conf is what you should change and
 # deploy if you want to do this.
 
-VENV=/opt/frp/environments/frp-app
-APP_ROOT=/opt/frp/deployed
-PID_FILE=/opt/frp/frp.pid
-ACCESS_LOG_FILE=/var/log/frp/frp-gunicorn.access.log
-ERROR_LOG_FILE=/var/log/frp/frp-gunicorn.error.log
+VENV=/home/infodigital/frp-env
+APP_ROOT=/home/infodigital/frp
+PID_FILE=/home/infodigital/frp/frp.pid
+ACCESS_LOG_FILE=/home/infodigital/log/frp-gunicorn.access.log
+ERROR_LOG_FILE=/home/infodigital/log/frp-gunicorn.error.log
 
 FRP_CONFIG=settings/production.py
 
@@ -21,16 +21,16 @@ FRP_CONFIG=settings/production.py
 case $1 in
    start)
       gunicorn --chdir ${APP_ROOT}/frp \
-	  -D \
-	  -p ${PID_FILE} \
-	  --access-logfile=${ACCESS_LOG_FILE} \
-	  --error-logfile=${ERROR_LOG_FILE} \
-	  --log-level=debug \
-	  wsgi:app
+          -D \
+          -p ${PID_FILE} \
+          --access-logfile=${ACCESS_LOG_FILE} \
+          --error-logfile=${ERROR_LOG_FILE} \
+          --log-level=debug \
+          frp:app
       ;;
-    stop)  
+    stop)
       kill `cat ${PID_FILE}` ;;
-    *)  
+    *)
       echo "usage: app.sh {start|stop}" ;;
 esac
 exit 0
