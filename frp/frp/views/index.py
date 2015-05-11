c16c1b25a6a18566a647b7a64277aca23e9682c5
Sairam Sudhir <sairamsudhir@mirafra.com>
2015-05-11 19:52:26 +0530

    At index page display featured campaigns , in case featured campaigns are less than 6  add other campaigns to the same also made changes in discover page to show SHOW_ALL button only in when a search has been done

--- frp/frp/views/index.py
+++ frp/frp/views/index.py
@@ -6,12 +6,25 @@ from .. import app
 from flask import session, g
 from ..forms import (FilterForm)
 
-from ..models import User, Donation
+from ..models import User, Donation,Campaign
 
 @app.route("/")
 def index():
     filter_form = FilterForm()
     total_books_donated = Donation.total_books_donated
+    campaigns_data = Campaign.all_campaigns_data()
+    campaigns = Campaign.query.all()
+    display_campaigns = filter(lambda x:x.featured==True,campaigns)
+    if len(campaigns_data)>=6 and len(display_campaigns)<6:
+        # add other campaigns till the number of campaigns  is 6
+        non_featured_campaigns = filter(lambda x:x.featured==False,campaigns)
+        i=0
+        while len(display_campaigns)<6:
+            display_campaigns.append(non_featured_campaigns[i])
+            i+=1
+
+
     return flask.render_template('index.html', 
             form=filter_form, 
-            total_books_donated=total_books_donated)
+            total_books_donated=total_books_donated,
+            display_campaigns=display_campaigns)
