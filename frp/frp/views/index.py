8525959b943169fd34504550bb0efc21599e7142
Sairam Sudhir <sairamsudhir@mirafra.com>
2015-05-12 15:28:28 +0530

    Added changes in index page to display Dummy campaigns is original cmapaigns are not present , Display same campaigns again when campaigns are less than 6

--- frp/frp/views/index.py
+++ frp/frp/views/index.py
@@ -12,19 +12,26 @@ from ..models import User, Donation,Campaign
 def index():
     filter_form = FilterForm()
     total_books_donated = Donation.total_books_donated
-    campaigns_data = Campaign.all_campaigns_data()
     campaigns = Campaign.query.all()
-    display_campaigns = filter(lambda x:x.featured==True,campaigns)
-    if len(campaigns_data)>=6 and len(display_campaigns)<6:
-        # add other campaigns till the number of campaigns  is 6
-        non_featured_campaigns = filter(lambda x:x.featured==False,campaigns)
-        i=0
+    display_campaigns = []
+    if len(campaigns)>=6:
+        display_campaigns = filter(lambda x:x.featured==True,campaigns)
+        if len(display_campaigns)<6:
+            # add other campaigns till the number of campaigns  is 6
+            non_featured_campaigns = filter(lambda x:x.featured==False,campaigns)
+            i=0
+            while len(display_campaigns)<6:
+                display_campaigns.append(non_featured_campaigns[i])
+                i+=1
+    elif len(campaigns)>0:
+        # Show the same campaigns again 
+        display_campaigns = campaigns
         while len(display_campaigns)<6:
-            display_campaigns.append(non_featured_campaigns[i])
+            display_campaigns.append(campaigns[i])
             i+=1
-
 
     return flask.render_template('index.html', 
             form=filter_form, 
             total_books_donated=total_books_donated,
             display_campaigns=display_campaigns)
+
