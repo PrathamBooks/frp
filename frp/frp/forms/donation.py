f773383e822ae19c07d71309435ce7c700d8573b
Pratham Books <infodigital@donateabook.org.in>
2019-02-02 18:28:07 +0530

    Add 5000 donation option

--- frp/frp/forms/donation.py
+++ frp/frp/forms/donation.py
@@ -5,7 +5,8 @@ import coaster
 from .. import app
 from .signup import (STATES)
 
-AMOUNT_CHOICES = [(2500, u'Rs. 2500 (62 books)'),
+AMOUNT_CHOICES = [(5000, u'Rs. 5000 (125 books or 1 Library-in-a-Classroom kit)'),
+                  (2500, u'Rs. 2500 (62 books)'),
                   (1000, u'Rs. 1000 (25 books)'),
                   (500, u'Rs. 500 (12 books)'),
                   (250, u'Rs. 250 (6 books)')]
